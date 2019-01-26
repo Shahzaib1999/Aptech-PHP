bd9e6222d9d7b09250ab3839423fb11676d47570
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-01-26 16:47:19 +0500

    Add files via upload

---
+++ Class 2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- <center><h1 style="font-size:<?php echo $a.'px' ?>; color:<?php echo '#'.$b ?>">ABC</h1></center> -->
+
+<?php
+// $a = rand(100,500);
+// $b = rand(000,1000);
+// echo 'Number: '.$a;
+
+?>
+    
+<form action="server.php" method="GET">
+    <input type="text" name="num" />     
+    <input type="submit" name="btn" value="Submit" />     
+</form>
+
+
+</body>
+</html>
