a9f2278f72606d041523f1622f84d031b8a89a98
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-03-19 16:49:44 +0500

    Add files via upload

--- Sending&gettingData_JS/city.php
+++ Sending&gettingData_JS/city.php
@@ -1,27 +1,44 @@
 <?php
 
+$con = new mysqli("localhost","root",null,"js");
 
-
-$server = "localhost";
-$username="root";
-$pass="";
-$dbname="1706d";
-
-
-$con = new mysqli($server,$username,$pass,$dbname);
 if($con) {
 	echo "connected successfully";
 }
 
 
+
+$a = $_GET['id5'];
+
 $qwe = $_GET['id3'];
 
-echo "<select><option> Select </option>"; 
-$asd1 = mysqli_query($con,"SELECT city_name from city where c_id=$qwe");
-while ($asad = mysqli_fetch_array($asd1)){
-	echo "<option>".$asad[0]." </option>";
+if ($qwe) {
+	echo "<select><option> Select </option>"; 
+
+$asd1 = mysqli_query($con,"SELECT Id,city_name from City where c_id=$qwe");
+
+	while ($asad = mysqli_fetch_array($asd1)){
+		echo "<option value='".$asad[0]."'>".$asad[1]." </option>";
+	}
+
+echo "</select>";	
 }
-echo "</select>";
+
+else if($a){
+	echo "<select><option> Select </option>"; 
+
+$q = mysqli_query($con,"SELECT Area_Name from area where City_Id=$a");
+
+	while ($b = mysqli_fetch_array($q)){
+		echo "<option>".$b[0]." </option>";
+	}
+
+echo "</select>";	
+}
+
+
+
+
 
 
 ?>
