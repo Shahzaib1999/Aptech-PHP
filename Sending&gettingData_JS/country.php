b56ed158b3d24276d2f714297aca58d78bfc0dad
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-03-19 16:50:24 +0500

    Add files via upload

--- Sending&gettingData_JS/country.php
+++ Sending&gettingData_JS/country.php
@@ -1,81 +1,67 @@
+<!DOCTYPE html>
+
 <?php
 
+$con = new mysqli("localhost","root",null,"js");
 
-$server = "localhost";
-$username="root";
-$pass="";
-$dbname="1706d";
-
-
-$con = new mysqli($server,$username,$pass,$dbname);
-if($con) {
-	echo "connected successfully";
-}
+// if($con) {
+// 	echo "connected successfully";
+// }
 ?>
 
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
 <body>
 Select Country :
   <select id="id1" onchange="myfun()">  
 
-  
+    <option>  Select Country </option>
 
+  <?php
 
+    $kal = mysqli_query($con,"SELECT * FROM country");
+    while ($kal1 = mysqli_fetch_array($kal)){
+       echo "<option value='".$kal1[0]."'> ".$kal1[1]."</option>";
 
+    }
 
-
-  <option>  select </option>
-<?php
-
-
-$kal = mysqli_query($con,"SELECT * FROM country");
-while ($kal1 = mysqli_fetch_array($kal)){
-   echo "<option value='".$kal1[0]."'> ".$kal1[1]."</option>";
-
-}
-
-?>
+  ?>
 
   </select>
 
+<br /><br />
 
-<br> <br>
+Select City :
+<select id="id2" onchange="area()">
+	<option>Select City</option>
+</select>
 
-<select id="id2">
-	<option>Select</option>
+<br /> <br/>
 
-
-
-
-
+Select Area :
+<select id="id4">
+  <option>Select Area</option>
 </select>
 
 
-</body>
-</html>
-
-
 <script type="text/javascript">
-	function myfun(){
+  function myfun(){
      var asd = new XMLHttpRequest();
      asd.open ("GET", "city.php?id3="+document.getElementById('id1').value, false);
      asd.send('null');
      document.getElementById('id2').innerHTML = asd.responseText; 
 
+  }
 
+  function area(){
+    debugger
+     var aa = new XMLHttpRequest();
+     aa.open ("GET", "city.php?id5="+document.getElementById('id2').value, false);
+     aa.send('null');
+     document.getElementById('id4').innerHTML = aa.responseText; 
 
-
-
-	}
-
-
-
-
-
+  }
 
 </script>
+</body>
+</html>
+
+
